77a5e4efb5aad90cdad9c557389ae50478b779ed
GenWatt <adrian.nowy@op.pl>
2019-11-11 19:39:02 +0100

    Updates

---
+++ precache-manifest.637910bf61c74da5086f5d772bd4c63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdcd62516ae99c2601ee703b0c4cc18",
+    "url": "/Computer-Shop/index.html"
+  },
+  {
+    "revision": "f9d1e1ee9c18f541458e",
+    "url": "/Computer-Shop/static/css/main.c8f06834.chunk.css"
+  },
+  {
+    "revision": "adb1cdb411cd3e3d9e7b",
+    "url": "/Computer-Shop/static/js/2.515798b0.chunk.js"
+  },
+  {
+    "revision": "f9d1e1ee9c18f541458e",
+    "url": "/Computer-Shop/static/js/main.18bf75df.chunk.js"
+  },
+  {
+    "revision": "c798b9d580dd9ab6b13f",
+    "url": "/Computer-Shop/static/js/runtime-main.0384f62a.js"
+  },
+  {
+    "revision": "d0fe4a171782dcb3d8239483bb570480",
+    "url": "/Computer-Shop/static/media/speed.d0fe4a17.jpg"
+  }
+]);
